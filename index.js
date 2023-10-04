0f0a8c600d4a082b885bb30ca755b931528017eb
muhamadarjun17 <144690217+muhamadarjun17@users.noreply.github.com>
2023-10-05 06:56:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,9 @@
-/*
-Copyright (c) 2013 dissimulate at codepen
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-
-/* Customisable map data */
 
 var map = {
 
     tile_size: 16,
 
-    /*
-    
-    Key vairables:
-    
-    id       [required] - an integer that corresponds with a tile in the data array.
-    colour   [required] - any javascript compatible colour variable.
-    solid    [optional] - whether the tile is solid or not, defaults to false.
-    bounce   [optional] - how much velocity is preserved upon hitting the tile, 0.5 is half.
-    jump     [optional] - whether the player can jump while over the tile, defaults to false.
-    friction [optional] - friction of the tile, must have X and Y values (e.g {x:0.5, y:0.5}).
-    gravity  [optional] - gravity of the tile, must have X and Y values (e.g {x:0.5, y:0.5}).
-    fore     [optional] - whether the tile is drawn in front of the player, defaults to false.
-    script   [optional] - refers to a script in the scripts section, executed if it is touched.
-    
-    */
     
     keys: [
         {id: 0, colour: '#333', solid: 0},
